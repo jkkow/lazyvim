51811fd54571ffa7658b78ca87245dadbb3a143d
jkko_yoga <jkwonko@gmail.com>
2025-12-14 15:36:28 +0900

    update options.lua for clipboard syncing

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,3 +21,9 @@ opt.scrolloff = 15
 -- Every wrapped line will continue visually indented (same amount of space as the beginning of that line), thus preserving horizontal blocks of text.
 opt.breakindent = true
 opt.autoindent = true
+
+-- Sync clipboard between OS and Neovim.
+-- vim.schedule let this setup rusn after UiEnter.
+vim.schedule(function()
+  vim.opt.clipboard = "unnamedplus"
+end)
