74586aee27c2b9542c1d76032389bfe556de43d7
jkkow@dtksp <jkwonko@gmail.com>
2026-03-04 14:51:29 +0900

    change some keymap for opencode

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -3,7 +3,7 @@ return {
   version = "*", -- Latest stable release
   dependencies = {
     -- Recommended for 'ask()', and required for 'toggle()' -- otherwise optional
-    { "folke/snacks.nvim", opts = { input = { enabled = true } }, },
+    { "folke/snacks.nvim", opts = { input = { enabled = true } } },
   },
   config = function()
     vim.g.opencode_opts = {
@@ -14,13 +14,17 @@ return {
     vim.o.autoread = true
 
     -- Recommended/example keymaps
-    vim.keymap.set({ "n", "t" }, "<leader>ot", function() require("opencode").toggle() end,
-      { desc = "Toggle opencode" })
-    vim.keymap.set({ "n", "t" }, "<leader>oa", function() require("opencode").ask("@cursor: ") end,
-      { desc = "Opencode ask this" })
-    vim.keymap.set({ "v" }, "<leader>oa", function() require("opencode").ask("@selection: ") end,
-      { desc = "Opencode ask selection" })
-    vim.keymap.set({ "n", "x" }, "go", function() return require("opencode").operator("@this ") end,
-      { desc = "Add range to opencode", expr = true })
+    vim.keymap.set({ "n", "t" }, "<leader>ot", function()
+      require("opencode").toggle()
+    end, { desc = "Toggle opencode" })
+    vim.keymap.set({ "n", "t" }, "<leader>oa", function()
+      require("opencode").ask("@cursor: ")
+    end, { desc = "Opencode ask this" })
+    vim.keymap.set({ "v" }, "<leader>oa", function()
+      require("opencode").ask("@selection: ")
+    end, { desc = "Opencode ask selection" })
+    vim.keymap.set({ "n", "x" }, "<leader>og", function()
+      return require("opencode").operator("@this ")
+    end, { desc = "Add range to opencode", expr = true })
   end,
 }
