57a71b928fe9936524ec89b556c4c359604ce10a
jkko <jkwonko@gmail.com>
2026-04-09 14:20:37 +0900

    feat(opencode0: Update opencode plugin setup
    - set shorcuts to exit opecode buffer(terminal): 'lk' and 'ESC ESC'\n- fix opencode ask command to properly deliver line info to opencode buffer

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -18,12 +18,19 @@ return {
       require("opencode").toggle()
     end, { desc = "Toggle opencode" })
     vim.keymap.set({ "n", "t" }, "<leader>oa", function()
-      require("opencode").ask("@cursor: ")
+      local ctx = string.format("@cursor %s:%d::: ", vim.fn.expand("%"), vim.fn.line("."))
+      require("opencode").ask(ctx)
     end, { desc = "Opencode ask this" })
     vim.keymap.set({ "v" }, "<leader>oa", function()
-      require("opencode").ask("@selection: ")
+      local line1 = vim.fn.line("v")
+      local line2 = vim.fn.line(".")
+      if line1 > line2 then
+        line1, line2 = line2, line1
+      end
+      local ctx = string.format("@selection %s:%d-%d::: ", vim.fn.expand("%"), line1, line2)
+      require("opencode").ask(ctx)
     end, { desc = "Opencode ask selection" })
-    vim.keymap.set({ "n", "x" }, "<leader>og", function()
+    vim.keymap.set({ "n", "x" }, "go", function()
       return require("opencode").operator("@this ")
     end, { desc = "Add range to opencode", expr = true })
   end,
