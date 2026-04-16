f589b23c1b8cf40e3c821aa7efb2753f6742453c
jkkow@dtksp <jkwonko@gmail.com>
2026-04-16 11:13:56 +0900

    fix(options): guard pwsh shell configuration on Windows

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,4 +37,19 @@ opt.linebreak = true -- Wrap at word boundaries
 -- GUI font for Windows Neovim clients (Neovide, nvim-qt, etc.)
 if vim.fn.has("win32") == 1 then
   opt.guifont = "JetBrainsMono Nerd Font:h11"
+  if vim.fn.executable("pwsh") == 1 then
+    opt.shell = "pwsh"
+
+    opt.shellcmdflag = table.concat({
+      "-NoLogo",
+      "-NoProfile",
+      "-ExecutionPolicy RemoteSigned",
+      "-Command",
+      "[Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+    }, " ")
+    opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    opt.shellquote = ""
+    opt.shellxquote = ""
+  end
 end
